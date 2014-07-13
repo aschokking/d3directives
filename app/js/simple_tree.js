93e7431ec2b350e2240eccdd102c1d718ab5ced2
Alex Schokking <aschokking@gmail.com>
2014-07-13 13:58:49 -0700

    basic layout and pan+zoom working

--- app/js/simple_tree.js
+++ app/js/simple_tree.js
@@ -1,25 +1,54 @@
 // Get JSON data
 treeJSON = d3.json("flare.json", function(error, treeData) {
-  var size = [800, 400];
+  
+  // Define the zoom function for the zoomable tree
+  function zoom() {
+      layoutRoot.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  }
+
+
+  // define the zoomListener which calls the zoom function on the "zoom" event constrained within the scaleExtents
+  var zoomListener = d3.behavior.zoom().scaleExtent([0.1, 3]).on("zoom", zoom);
+  
+  // size of the diagram
+  var viewerWidth = $(document).width();
+  var viewerHeight = $(document).height();
+  
+  var getChildren = function(d) {
+    return (d.children && d.children.length > 0) ? d.children : null;
+  };
+  
+  // computer number of child nodes
+  var totalLeafs = 0;
+  treeData.children.forEach(function(d) {
+    totalLeafs += d.children ? d.children.length : 0;
+  });
+  
+  var viewHieght = totalLeafs * 25;
   
   var tree = d3.layout.tree()
-    .children(function(d) {
-      return (d.children && d.children.length > 0) ? d.children : null;
-    })
-    .size(size);
+    .children(getChildren)
+    .size([viewHieght, viewerWidth]);
 
   var nodes = tree.nodes(treeData);
   var links = tree.links(nodes);
   
   var layoutRoot = d3.select("#tree-container")
-    .append("svg:svg").attr("width", size.width).attr("height", size.height)
+    .append("svg:svg").attr("width", viewerWidth).attr("height", viewerHeight)
+    .attr("class", "overlay")
+    .call(zoomListener)
     .append("svg:g");
     
   var link = d3.svg.diagonal()
     .projection(function(d) {
       return [d.y, d.x];
     });
-    
+  
+  // Set widths between levels based on maxLabelLength.
+  nodes.forEach(function(d) {
+    d.y = (d.depth * 250); //500px per level.
+  });
+  
   layoutRoot.selectAll("path.link")
     .data(links)
     .enter()
@@ -37,5 +66,8 @@ treeJSON = d3.json("flare.json", function(error, treeData) {
     });
     
   nodeGroup.append("svg:circle")
-    .attr("class", "node-dot");
+    .attr("class", "node-dot")
+    .attr("r", 4.5);
+    
+  
 });
